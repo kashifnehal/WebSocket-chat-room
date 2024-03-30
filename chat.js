0fa4290ce85c5322df090aabcd5bb500f0f6b8d5
kashifNehal1997 <knehal@chegg.com>
2024-03-30 16:42:29 +0530

    commiting to test mac

--- chat.js
+++ chat.js
@@ -10,24 +10,24 @@ var feedback = document.getElementById('feedback');
 
 //EMIT EVENTS
 
-btn.addEventListener('click',function(){
-    socket.emit('chat',{
-        message:message.value,
-        handle:handle.value
+btn.addEventListener('click', function () {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
     })
 })
 
-message.addEventListener('focus',function(){
-    socket.emit('typing',handle.value);
+message.addEventListener('focus', function () {
+    socket.emit('typing', handle.value);
 })
 
 //Listen for events
-socket.on('chat',function(data){
+socket.on('chat', function (data) {
     feedback.innerHTML = ""
     output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>'
 })
 
-socket.on('typing',function(data){
-    feedback.innerHTML += '<p><em>' + data + ' is typing a message...</em></p>';
+socket.on('typing', function (data) {
+    feedback.innerHTML += '<p><em>' + data + ' is typing...</em></p>';
 })
 
